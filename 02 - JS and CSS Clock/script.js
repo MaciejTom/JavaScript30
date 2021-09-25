83cea0088faded5e1def3da9d0c03eeae57c3e1a
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2021-09-25 11:57:24 +0200

    current date added

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,8 +1,49 @@
-const minute = document.querySelector(".hand") 
-    minute.style.transformOrigin = "100%";
-    let second = 0;
-    
-    setInterval(() => {
-        console.log("asdasd")
-        minute.style.transform = `rotate(${second += 6}deg)`
-    },1000)
+let second = document.querySelector(".second-hand");
+hour = document.querySelector(".hour-hand"),
+    minute = document.querySelector(".min-hand"),
+    hands = document.querySelectorAll(".hand");
+
+
+
+const setDate = function () {
+
+    let now = new Date();
+
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let hours = now.getHours();
+    // console.log(seconds)
+    console.log(minutes)
+
+    second.style.transform = `rotate(${seconds * 6 + 90}deg)`;
+    minute.style.transform = `rotate(${minutes * 6 + 90}deg)`;
+    hour.style.transform = `rotate(${hours * 6 + 90}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+
+// let seconds = 90,
+//     minutes = 90,
+//     hours = 90;
+
+// let now = new Date();
+// console.log(now)
+// console.log(now.getMinutes())
+
+// setInterval(() => {
+
+//     second.style.transform = `rotate(${seconds += 6}deg)`
+// }, 100);
+
+// setInterval(() => {
+
+//     minute.style.transform = `rotate(${minutes += 6}deg)`
+//     console.log("zaczynamy odliczanie")
+// }, 6000);
+
+// setInterval(() => {
+
+//     hour.style.transform = `rotate(${hours += 6}deg)`
+// }, 3600000);
+
